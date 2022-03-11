32814b4ae5ef09b2891b54938dbeb03f89fa7930
mdreed2 <meshia.d.reed@gmail.com>
2022-03-10 22:42:44 -0700

    commented out emailjs import

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,5 +1,5 @@
 import React, { useRef } from 'react'
-import emailjs from '@emailjs/browser'
+// import emailjs from '@emailjs/browser'
 import './contact.scss'
 import { MdOutlineEmail } from 'react-icons/md'
 import { AiOutlineWhatsApp } from 'react-icons/ai'
